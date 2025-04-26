6693cf8500c32de6b684aed80442268eb78a2218
vishnu <vishnuprasad.vp325@gmail.com>
2025-04-26 14:33:13 +0530

    Two tasks done

---
+++ scripts/Task1.py
@@ -0,0 +1,8 @@
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+
+print("Addition :",x+y)
+print("Substraction :",x-y)
+print("Multiplication :",x*y)
+print("Division :",x/y)
+
---
+++ scripts/Task2.py
@@ -0,0 +1,3 @@
+First_name = input("Enter the first name:")
+second_name = input("Enter the second name:")
+print("Hello",First_name+second_name,"! Welcome to course")
