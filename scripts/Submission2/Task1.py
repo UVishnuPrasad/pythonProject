8544123f1a12de3280e580db687e13cbbc9e7e44
vishnu <vishnuprasad.vp325@gmail.com>
2025-04-26 20:11:14 +0530

    new dir and tasks added

---
+++ scripts/Submission2/Task1.py
@@ -0,0 +1,6 @@
+from traceback import print_tb
+Input = int(input("Enter a value to check"))
+if Input%2 ==0 :
+    print(Input,"is a even number")
+else:
+    print(Input,"is a odd number")
---
+++ scripts/Submission2/Task2.py
@@ -0,0 +1,4 @@
+Sum =0
+for i in range(1,51):
+    Sum +=i
+print("the sum of numbers from 1 to 50 is:",Sum)
